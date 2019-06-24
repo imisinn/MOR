8a7c194125242431a6883b4341d285d1255ae54b
井水真也 <>
2019-06-24 17:58:03 +0900

    can use AdLint

--- free.sh
+++ free.sh
@@ -5,6 +5,10 @@ if [ $# -ne 1 ]; then
   echo "ファイルを1つ指定してください"
 elif [[ $1 =~ .*\.c ]]; then
   echo "c言語のファイルです"
+  java delete_inc $1
+  adlintize -o adlint
+  cd adlint
+  make verbose-all
 else
   echo "c言語のファイルを用いてください"
 fi
