2ade9ad8d99c5887487af4aa7dfff70f3bb34f48
imisinn <shinnya19971027@gmail.com>
2019-09-04 00:59:07 +0900

    create check_quality.java

---
+++ check_quality.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+public class check_quality{
+  void run(String[] args)throws IOException{
+  }
+
+  public static void main(String[] args)throws IOException{
+    check_quality app = new check_quality();
+    app.run(args);
+  }
+}
