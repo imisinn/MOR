42cc1bf36cddb6a6331af64834a76d2e2270fa6e
imisinn <shinnya19971027@gmail.com>
2019-09-23 00:01:42 +0900

    単体のコードに対して解析ができるシェルスクリプトの追加

---
+++ run2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# １つのファイルを解析するため
+if [ $# -ne 1 ]; then
+  echo "ファイルを1つ指定してください"
+elif [[ $1 =~ .*\.c ]]; then
+  echo "c言語のファイルです"
+  mkdir free
+  cp $1 free/$1
+  cd free
+  java -classpath ../ delete_inc $1
+  adlintize -o adlint
+  java -classpath ../ change_langage
+  cd adlint
+  rm adlint_traits.yml
+  mv hoge.yml adlint_traits.yml
+  make verbose-all
+  cd ../
+  java -classpath ../ pickup_error $1
+  clang -cc1 -ast-dump $1 &> AST.txt
+else
+  echo "c言語のファイルを用いてください"
+fi
